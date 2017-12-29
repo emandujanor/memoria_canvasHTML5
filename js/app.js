16655b08d46c02a97a2ed6a0cb6bd913d385807f
Edith Mandujano <edithmandujano001@gmail.com>
2017-12-29 01:02:25 -0600

    Se modifica el constructor usando un prototipo

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 /**VARIABLES***/
-var ctx;
 const canvas = document.getElementById("miCanvas");
-var primerCarta = true;
-var cartaPrimera, cartaSegunda;
-var colorDelante = "yellow";
+const ctx = canvas.getContext("2d");
+let primerCarta = true;
+let cartaPrimera, cartaSegunda;
+let colorDelante = "yellow";
 var colorAtras = "blue";
 var colorCanvas = "green";
 var inicioX = 45;
@@ -20,42 +20,44 @@ var cartas = 0;
 
 
 /***FUNCIONES****/
-//Crea el objeto Carta
-function Carta(x, y, ancho, largo, info){
-  this.x = x;
-  this.y = y;
-  this.ancho = ancho;
-  this.largo = largo;
-  this.info = info;
-  this.dibuja = dibujaCarta;
+//Crea el objeto Carta implementando un prototipo
+class Carta{
+  constructor(x, y, ancho, largo, info){
+    this.x = x;
+    this.y = y;
+    this.ancho = ancho;
+    this.largo = largo;
+    this.info = info;
+  }
+  dibujarCarta(){
+     ctx.fillStyle = colorAtras;
+     ctx.fillRect(this.x, this.y, this.ancho, this.largo);
+   }
 }
-function dibujaCarta(){
-  ctx.fillStyle = colorAtras;
-  ctx.fillRect(this.x, this.y, this.ancho, this.largo);
-}
-function tablero(){
- var i;
- var carta;
- var x = inicioX;
- var y = inicioY;
- for(i = 0; i < 6; i++){
+
+//dibuja cada una de las cartas dentro del tablero
+const tablero = () => {
+ let carta;
+ let x = inicioX;
+ let y = inicioY;
+ for( let i = 0; i < 6; i++){
   carta = new Carta(x, y, cartaAncho, cartaLargo, i);
   cartas_array.push(carta);
-  carta.dibuja();
+  carta.dibujarCarta();
   //Creamos la segunda carta
   carta = new Carta(x, y + cartaAncho + cartaMargen, cartaAncho, cartaLargo, i);
   cartas_array.push(carta);
-  carta.dibuja();
+  carta.dibujarCarta();
   //Aumentamos el valor de x
   x += cartaAncho + cartaMargen;
  }
-
+ //console.log(cartas_array);
 }
 /*intercambia las cartas de lugar*/
-function barajear(){
-  var i, j, k;
-  var temporal;
-  var lon = cartas_array.length;
+const barajear = () => {
+  let i, j, k;
+  let temporal;
+  const lon = cartas_array.length;
   for(j=0; j<lon*3; j++){
     i = Math.floor(Math.random()*lon);
     k = Math.floor(Math.random()*lon);
@@ -64,15 +66,14 @@ function barajear(){
     //
     cartas_array[i].info = cartas_array[k].info;
     cartas_array[k].info = temporal;
-
   }
 }
 /*Nos da las coordenadas exactas que fueron cliqueadas*/
 function ajusta(xx, yy){
-var posCanvas = canvas.getBoundingClientRect();/*nos da las coordenadas absolutas del canvas, largo y alto que fue clickeado*/
-var x = xx -posCanvas.left;//nos da la posicion x y y con repecto a la posicion original del canvas
-var y = yy -posCanvas.top;
-return {x:x, y:y}
+  const posCanvas = canvas.getBoundingClientRect();/*nos da las coordenadas absolutas del canvas, largo y alto que fue clickeado*/
+  let x = xx -posCanvas.left;//nos da la posicion x y y con repecto a la posicion original del canvas
+  let y = yy -posCanvas.top;
+  return {x:x, y:y}
 }
 
 function selecciona(e){
@@ -122,8 +123,8 @@ function selecciona(e){
 
 function volteaCarta(){
   if(iguales==false){
-    cartas_array[cartaPrimera].dibuja();
-    cartas_array[cartaSegunda].dibuja();
+    cartas_array[cartaPrimera].dibujarCarta();
+    cartas_array[cartaSegunda].dibujarCarta();
   }else{//Eliminamos las cartas
     ctx.clearRect(cartas_array[cartaPrimera].x, cartas_array[cartaPrimera].y, cartas_array[cartaPrimera].ancho, cartas_array[cartaPrimera].largo);
     ctx.clearRect(cartas_array[cartaSegunda].x, cartas_array[cartaSegunda].y, cartas_array[cartaSegunda].ancho, cartas_array[cartaSegunda].largo);
@@ -137,15 +138,15 @@ function volteaCarta(){
 //Dibuja la carta, darle la vuelta
 function pinta(carta){//carta es el indice dentro de nuestro arreglo
   ctx.fillStyle = colorDelante;
-ctx.fillRect(carta.x, carta.y, carta.ancho, carta.largo);
-ctx.font = "bold 40px Comic";
-ctx.fillStyle = "black";
-ctx.fillText(String(carta.info), carta.x+carta.ancho/2-10, carta.y + carta.largo/2+10);
+  ctx.fillRect(carta.x, carta.y, carta.ancho, carta.largo);
+  ctx.font = "bold 40px Comic";
+  ctx.fillStyle = "black";
+  ctx.fillText(String(carta.info), carta.x+carta.ancho/2-10, carta.y + carta.largo/2+10);
 
 }
 function aciertos(){
   ctx.save();
-ctx.fillStyle ="black";
+  ctx.fillStyle ="black";
 if( cartas==6){
    ctx.clearRect(0,0, canvas.width, canvas.height);
    ctx.font = "bold 80px Comic";
@@ -163,10 +164,7 @@ if( cartas==6){
 }
 /* INICIO*/
 window.onload = () => {
-
-  if(canvas && canvas.getContext)  {
-    ctx = canvas.getContext("2d");
-    console.log(canvas, ctx);
+  if(canvas && canvas.getContext){
     if(ctx){
       canvas.addEventListener("click", selecciona, false);
       tablero();
